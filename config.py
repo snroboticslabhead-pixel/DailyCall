54ab7961e4c6b670764d7b360c10f3ad9aab498a
snroboticslabhead-pixel <snroboticslabhead@gmail.com>
2025-11-21 23:50:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret-key")
-    MONGO_URI = os.environ.get(
-        "MONGO_URI",
-        "mongodb://localhost:27017/attendance_db"
-    )
+    
+    # MySQL configuration for PythonAnywhere
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'DailyCall.mysql.pythonanywhere-services.com'
+    MYSQL_USER = os.environ.get('MYSQL_USER') or 'DailyCall'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'Krishna@532'
+    MYSQL_DB = os.environ.get('MYSQL_DB') or 'DailyCall$default'
 
     # Simple single admin user (you can later store in DB)
     ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
